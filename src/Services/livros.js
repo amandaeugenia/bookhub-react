da636e4d47f15d60b36f4169fc55fc367f895ee6
Amanda Eugenia <amandaeugenia88@gmail.com>
2023-06-19 23:18:12 -0300

    final

--- src/Services/livros.js
+++ src/Services/livros.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const livrosAPI = axios.create({baseURL: "http://localhost:8000/livros"})
 
 async function getLivros() {
-    const response = await livrosAPI.get('/')
+    const response = await livrosAPI.get('/:')
 
     return response.data
 }
